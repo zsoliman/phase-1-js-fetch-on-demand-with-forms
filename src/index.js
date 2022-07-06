e67a61bdd80fe344f661d02b12ffb1fa50db60de
Zachary Soliman <zachsoliman1@gmail.com>
2022-07-05 22:38:27 -0400

    able to search by ID

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+
+        // event.target.children[1].value //gets the same info as vv
+        const input = document.querySelector('input#searchByID')
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())  //if fetch succeeds, take info as response and turn it into a json
+            .then(data => { //rename json info as data
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
